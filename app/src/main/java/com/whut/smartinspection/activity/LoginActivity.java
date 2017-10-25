b3c05bdf54dd4f32caf8edc1c0d0d9436d0a33f2
whut-bin <751743289@qq.com>
2017-10-25 16:33:22 +0800

    finish login

--- app/src/main/java/com/whut/smartinspection/activity/LoginActivity.java
+++ app/src/main/java/com/whut/smartinspection/activity/LoginActivity.java
@@ -5,37 +5,81 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.whut.smartinspection.R;
+import com.whut.smartinspection.component.handler.EMsgType;
+import com.whut.smartinspection.component.handler.IHandlerListener;
+import com.whut.smartinspection.component.http.UserComponent;
+import com.whut.smartinspection.utils.SystemUtils;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
-public class LoginActivity extends Activity {
-	
-	@BindView(R.id.tv_login_user_login)
-	Button tvUserLogin;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-		
-	}
-	
-	@OnClick(R.id.tv_login_user_login)
-	public void onClick(View view) {
-		Intent intent;
-		switch (view.getId()) {
-		// 登录
-		case R.id.tv_login_user_login:
-			intent = new Intent(this, MainActivity.class);
-			startActivity(intent);
-			break;
+public class LoginActivity extends Activity implements IHandlerListener {
 
-		default:
-			break;
-		}
-	}
+    @BindView(R.id.btn_login_user_login)
+    Button btnUserLogin;
+
+    @BindView(R.id.et_login_username)
+    EditText etLoginUsername;
+
+    @BindView(R.id.et_login_password)
+    EditText etLoginPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login_user_login, R.id.btn_login_reset, R.id.btn_login_user_exit})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            // 登录
+            case R.id.btn_login_user_login:
+                login();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void login() {
+        String username = etLoginUsername.getText().toString().trim();
+        String password = etLoginPassword.getText().toString().trim();
+        UserComponent.login(this, username, password);
+    }
+
+    @Override
+    public void onSuccess(Object obj, EMsgType type) {
+        switch (type) {
+            // 登录成功
+            case LOGIN_SUCCESS:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+
+            default:
+                break;
+
+        }
+    }
+
+    @Override
+    public void onFailure(Object obj, EMsgType type) {
+        switch (type) {
+            // 登录失败
+            case LOGIN_FAILURE:
+                SystemUtils.showToast(this, obj.toString());
+                break;
+
+            default:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/whut/smartinspection/component/http/UserComponent.java
+++ app/src/main/java/com/whut/smartinspection/component/http/UserComponent.java
@@ -1,6 +1,8 @@
 package com.whut.smartinspection.component.http;
 
+import com.whut.smartinspection.component.handler.EMsgType;
 import com.whut.smartinspection.component.handler.IHandlerListener;
+import com.whut.smartinspection.parser.CustomParser;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -17,23 +19,30 @@ public class UserComponent extends BaseHttpComponent {
     /***
      * 登录
      * @param listener
-     * @param userName
+     * @param username
      * @param password
      */
-    public static void login(IHandlerListener listener, String userName, String password) {
+    public static void login(final IHandlerListener listener, String username, String password) {
 
-        OkHttpUtils.get().
-                url("").
-                addParams("userName", userName).
+        String url = "http://172.19.137.30:8080/BDZXJService/android/login";
+        OkHttpUtils.post().
+                url(url).
+                addParams("username", username).
                 addParams("password", password).
                 build().execute(new StringCallback() {
             @Override
             public void onError(Call call, Exception e, int id) {
-
+                listener.onFailure(e.getMessage(), EMsgType.LOGIN_FAILURE);
             }
 
             @Override
             public void onResponse(String response, int id) {
+                CustomParser.ResponseObject ro = CustomParser.parse(response);
+                if (ro.getRet() == 200) {
+                    listener.onSuccess(ro.getMsg(), EMsgType.LOGIN_SUCCESS);
+                } else {
+                    listener.onFailure(ro.getMsg(), EMsgType.LOGIN_FAILURE);
+                }
 
             }
         });
